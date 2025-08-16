edec6d56fc08416911c018626267e2cfddba1417
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-08-16 16:31:09 +0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,1 +1,26 @@
 import './styles.css';
+
+
+async function getWeather(location) {
+    try {
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=PTYBGPYCANW6933CP9D3MMW9A`
+);
+
+        if (!response.ok) {
+            throw new Error(`HTTP error! status: ${response.status}`)
+        }
+
+        const data = await response.json();
+        console.log(data);
+        return data;
+    } catch(error) {
+        console.error(error)
+    }
+}
+
+const search = document.getElementById('location-search');
+const button = document.getElementById('searchBtn');
+
+button.addEventListener('click', () => {
+    getWeather(search.value.toLowerCase())
+});
