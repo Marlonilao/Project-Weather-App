e0f55f62751b4c0a53271c5d282ade98fdf75fa1
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-08-17 12:27:18 +0800

    wip

--- src/index.js
+++ src/index.js
@@ -1,26 +1,25 @@
 import './styles.css';
 
+const search = document.getElementById('location-search');
+const button = document.getElementById('searchBtn');
+const output = document.getElementById('weather-container')
 
 async function getWeather(location) {
     try {
-        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?key=PTYBGPYCANW6933CP9D3MMW9A`
-);
-
+        const response = await fetch(`https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=PTYBGPYCANW6933CP9D3MMW9A`);
         if (!response.ok) {
             throw new Error(`HTTP error! status: ${response.status}`)
-        }
-
+        }   
         const data = await response.json();
-        console.log(data);
-        return data;
+        return data
     } catch(error) {
         console.error(error)
+        return error
     }
 }
 
-const search = document.getElementById('location-search');
-const button = document.getElementById('searchBtn');
-
 button.addEventListener('click', () => {
-    getWeather(search.value.toLowerCase())
+    getWeather(search.value).then(response => {
+        console.log(response, response.currentConditions, response.days[1], response.days[2], response.days[3]);
+    })
 });
