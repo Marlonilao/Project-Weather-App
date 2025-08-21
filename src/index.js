a911a6d86dbb3f15d920feaf63f5618f94a46207
Marlonilao <176732593+Marlonilao@users.noreply.github.com>
2025-08-21 12:58:58 +0800

    finish project

--- src/index.js
+++ src/index.js
@@ -6,17 +6,14 @@ const form = document.querySelector('form');
 const weatherContainer = document.querySelector('#weather-container');
 
 async function getWeather(location) {
-  try {
-    const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=PTYBGPYCANW6933CP9D3MMW9A`,
-    );
-    if (!response.ok) {
-      throw new Error(`HTTP error! status: ${response.status}`);
-    }
+  const response = await fetch(
+    `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${location}?unitGroup=metric&key=PTYBGPYCANW6933CP9D3MMW9A`,
+  );
+  if (!response.ok) {
+    throw new Error(`HTTP error! status: ${response.status}`);
+  } else {
     const data = await response.json();
     return data;
-  } catch (error) {
-    renderError(error);
   }
 }
 
@@ -25,31 +22,25 @@ form.addEventListener('submit', (e) => {
     return;
   }
   e.preventDefault();
-  setTimeout(() => {
-    fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?unitGroup=metric&key=PTYBGPYCANW6933CP9D3MMW9A`,
-    )
-      .then((response) => {
-        if (!response.ok) {
-          throw new Error(`HTTP error! status: ${response.status}`);
-        }
-        return response.json();
-      })
-      .then((data) => {
-        renderCurrentWeather(data);
-      })
-      .catch((err) => {
-        renderError(err.message);
-      });
-  }, 1000);
-
+  // setTimeout(() => {
+  //   fetch(
+  //     `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${search.value}?unitGroup=metric&key=PTYBGPYCANW6933CP9D3MMW9A`,
+  //   )
+  //     .then((response) => {
+  //       if (!response.ok) {
+  //         throw new Error(`HTTP error! status: ${response.status}`);
+  //       }
+  //       return response.json();
+  //     })
+  //     .then((data) => {
+  //       renderCurrentWeather(data);
+  //     })
+  //     .catch((err) => {
+  //       renderError(err.message);
+  //     });
+  // }, 1000);
   weatherContainer.innerHTML = "<div class='loader'></div>";
-
-  // getWeather(search.value)
-  //   .then((response) => {
-  //     renderCurrentWeather(response);
-  //   })
-  //   .catch((response) => {
-  //     renderError(response);
-  //   });
+  getWeather(search.value)
+    .then((response) => renderCurrentWeather(response))
+    .catch((err) => renderError(err.message));
 });
